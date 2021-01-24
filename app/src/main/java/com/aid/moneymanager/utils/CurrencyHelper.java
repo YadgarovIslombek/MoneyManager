3385305aa86f0ee76f295fb87ade98668068c1c4
Islombek <islam.yadgarov.677@gmail.com>
2021-01-24 14:28:27 +0300

    24.01.21 14:24
    Signed-off-by: Islombek <islam.yadgarov.677@gmail.com>

--- app/src/main/java/com/aid/moneymanager/utils/CurrencyHelper.java
+++ app/src/main/java/com/aid/moneymanager/utils/CurrencyHelper.java
@@ -2,14 +2,14 @@
  * *
  *  * Created by Yadgarov Islombek on 2021
  *  * Copyright (c).  All rights reserved.
- *  * Last modified 21.01.21 23:26
+ *  * Last modified 24.01.21 14:24
  *  بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيم  *
  *
  */
 
 package com.aid.moneymanager.utils;
 
-import  android.text.Editable;
+import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,19 +17,27 @@ import android.widget.TextView;
 import com.aid.moneymanager.firebase.models.Currency;
 import com.aid.moneymanager.firebase.models.User;
 
+import java.text.NumberFormat;
+
 public class CurrencyHelper {
-    public static String formatCurrency(Currency curency, long money) {
-        long absMoney = Math.abs(money);
-        return (curency.left ? (curency.symbol + (curency.space ? " " : "")): "") +
-                (money < 0 ? "-" : "") + (absMoney / 100) + "." +
-                (absMoney % 100 < 10 ? "0" : "") +
-                (absMoney % 100)  +
-                (curency.left ? "" : ((curency.space ? " " : "") + curency.symbol));
+    public static String formatCurrency(Currency curency, long money) throws NumberFormatException {
+        //long absMoney = Math.abs(money);
+        NumberFormat numberFormat = NumberFormat.getInstance();
+
+//                (money < 0 ? "-" : "") + (absMoney / 1000.0) + "." +
+//                (absMoney % 100 < 10 ? "00" : "") +
+//                (absMoney % 100)  +
+        return (curency.left ? (curency.symbol + (curency.space ? " " : " ")) : " ") +
+                numberFormat.format(money) + (curency.left ? " " : ((curency.space ? " " : " ") + curency.symbol));
+
+
     }
+
     public static void setupAmountEditText(EditText editText, User user) {
-        editText.setText(CurrencyHelper.formatCurrency(user.currency,0), TextView.BufferType.EDITABLE);
+        editText.setText(CurrencyHelper.formatCurrency(user.currency, 0), TextView.BufferType.EDITABLE);
         editText.addTextChangedListener(new TextWatcher() {
             private String current = "";
+
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
             }
@@ -39,11 +47,10 @@ public class CurrencyHelper {
             public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
                 if (!charSequence.toString().equals(current)) {
                     editText.removeTextChangedListener(this);
-                    current = CurrencyHelper.formatCurrency(user.currency,convertAmountStringToLong(charSequence));
+                    current = CurrencyHelper.formatCurrency(user.currency, convertAmountStringToLong(charSequence));
                     editText.setText(current);
-                    editText.setSelection(current.length() -
-                            (user.currency.left ? 0 : (user.currency.symbol.length() + (user.currency.space ? 1 : 0))));
-
+                    editText.setSelection(current.length() - (user.currency.left ? 0 : (user.currency.symbol.length() + (user.currency.space ? 1 : 1))));
+//
                     editText.addTextChangedListener(this);
                 }
             }
